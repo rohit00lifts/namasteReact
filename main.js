b23898b7562849a90f30c460064098d0b35e3ea2
rohit00lifts <rt3317142@gmail.com>
2026-01-02 13:24:24 +0530

    moving all files to the inception folder

--- main.js
+++
@@ -1,7 +0,0 @@
-const mainElement = React.createElement
-('div',{id:'mainDiv'},
-    [React.createElement('div',{id:'child'},[React.createElement('h1',{},'I am h1 tag'),React.createElement('h2',{},'I am h2 tag')]),
-    React.createElement('div',{id:'child2'},[React.createElement('h1',{},'I am h1 tag'),React.createElement('h2',{},'I am h2 tag')])]
-    ,);
-const body = ReactDOM.createRoot(document.querySelector('body'));
-body.render(mainElement);
